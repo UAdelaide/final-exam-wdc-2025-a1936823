567c93f336ef74dbd8d1b6ed73b2e61f63d86026
a1936823 <lilokhang124@gmail.com>
2025-06-20 02:15:15 +0000

    [automatic commit] 2025-06-20 02:15:14 UTC

--- part1/app.js
+++ part1/app.js
@@ -7,5 +7,8 @@ const app = express();
 app.get('/api/dogs', (req, res) => {
     Pool.query(` SELECT Dogs.name AS dog_name FROM Dogs
                  JOIN Users ON Users.user_id = Dogs_owner.id`)
-    .then(([rows]) => res.json)
+    .then(([rows]) => res.json(rows))
+    .catch(err => {
+        console.
+    })
 })
