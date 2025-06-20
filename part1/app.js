84e71e11814dc68e40e69dc73cd9521c0ac6eedb
a1936823 <lilokhang124@gmail.com>
2025-06-20 02:20:37 +0000

    [automatic commit] 2025-06-20 02:20:37 UTC

--- part1/app.js
+++ part1/app.js
@@ -16,6 +16,6 @@ app.get('/api/dogs', (req, res) => {
 
 app.get('/api/walkrequests/open', (req, res) => {
     pool.query(` SELECT WalkRequests.request_id, Dogs.name AS dong_name, Requests.request_time, Requests.duration_minutes, Requests.location, Users.name AS owner_username
-                FROM WalkRequests 
+                FROM WalkRequests Requests JOIN Dogs Dogs
     )
 })
