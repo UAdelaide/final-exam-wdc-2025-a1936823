264da78516cd912809934d7b6e14635dc66c46b3
a1936823 <lilokhang124@gmail.com>
2025-06-20 02:38:36 +0000

    [automatic commit] 2025-06-20 02:38:35 UTC

--- part1/app.js
+++ part1/app.js
@@ -4,7 +4,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password
+    password: '',
+    database: 'Do'
  });
 const app = express();
 
