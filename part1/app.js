00fed919abafd0c248becdc9312ed47284d71067
a1936823 <lilokhang124@gmail.com>
2025-06-20 02:11:40 +0000

    [automatic commit] 2025-06-20 02:11:39 UTC

--- part1/app.js
+++ part1/app.js
@@ -5,5 +5,6 @@ import { Pool } from 'undici-types';
 const app = express();
 
 app.get('/api/dogs', (req, res) => {
-    Pool.query(` SELECT Dogs`)
+    Pool.query(` SELECT Dogs.name AS dog_name FROM Dogs
+                 JOIN Owners ON Dogs.owner_id = Owners.id`)
 })
