1d58471ad07f204ef046727718f6ddb88883a215
a1936823 <lilokhang124@gmail.com>
2025-06-20 02:10:13 +0000

    [automatic commit] 2025-06-20 02:10:13 UTC

--- part1/app.js
+++ part1/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
+import { Pool } from 'undici-types';
 
 const app = express();
 
-app.get('/api/dogs')
+app.get('/api/dogs', (req, res) => {
+    Pool.query
+})
