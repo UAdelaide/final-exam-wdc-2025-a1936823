5eb23301574824b33d41c872f98d204cc46e3eec
a1936823 <lilokhang124@gmail.com>
2025-06-20 02:28:52 +0000

    [automatic commit] 2025-06-20 02:28:51 UTC

--- part1/app.js
+++ part1/app.js
@@ -7,13 +7,14 @@ const app = express();
 app.get('/api/dogs', (req, res) => {
     Pool.query(` SELECT Dogs.name AS dog_name
                 FROM Dogs
-                 JOIN Users ON Users.user_id = Dogs_owner.id`);
+                JOIN Users ON Users.user_id = Dogs_owner.id`);
 });
 
 
 app.get('/api/walkrequests/open', (req, res) => {
     pool.query(` SELECT WalkRequests.request_id, Dogs.name AS dong_name, Requests.request_time, Requests.duration_minutes, Requests.location, Users.name AS owner_username
-                FROM WalkRequests Requests JOIN Dogs Dogs ON Dogs.dog_id = Requests.dog_id
+                FROM WalkRequests Requests
+                JOIN Dogs Dogs ON Dogs.dog_id = Requests.dog_id
                 WHERE Requests.status = 'open'
                 OREDER BY Resquests.requested_time;`);
 });
@@ -23,7 +24,8 @@ app.get('/api/walkers/summary', (req, res) => {
                 COUNT(ratings.rating_id) AS total_ratings,
                 ROUND (AVG(rating.rating), 2) AS average_rating,
                 COUNT(ratings.rating_id) AS completed_walks
-            FROM Users walkers LEFT JOIN WalkRatings ratings ON ratings.walker_id = walkers.user_id
+            FROM Users walkers
+            LEFT JOIN WalkRatings ratings ON ratings.walker_id = walkers.user_id
             WHERE walkers.role = 'walker' GROUP BY walkers.user_id GROUP BY walkers.username;
         `)
 })
