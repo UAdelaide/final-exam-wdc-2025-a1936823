8252aa48641ec53bc9e4891b2cb7337141953d20
a1936823 <lilokhang124@gmail.com>
2025-06-21 03:58:32 +0000

    [automatic commit] 2025-06-21 03:58:32 UTC

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'DogWalkService',
+  database: 'Users',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
