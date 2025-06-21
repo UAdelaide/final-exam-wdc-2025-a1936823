c27d6576072c9cd6ab3d25fb80b95f6124e928f7
a1936823 <lilokhang124@gmail.com>
2025-06-21 06:11:21 +0000

    [automatic commit] 2025-06-21 06:11:21 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,5 +5,9 @@ const session = require('express-session');
 
 router.get('/mine', (req, res) => {
     if (!req.session.user || req.session.user.role !== 'owner')
-        return res.status(401).json({ error: 'Not authorized' })
+        return res.status(401).json({ error: 'Not authorized' });
+
+    db.query(
+        
+    )
 })
