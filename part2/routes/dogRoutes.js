11f6b838b55eb8afab4cbdb1205c48a55fb1ff14
a1936823 <lilokhang124@gmail.com>
2025-06-21 06:14:59 +0000

    [automatic commit] 2025-06-21 06:14:58 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const session = require('express-session');
 
 router.get('/mine', (req, res) => {
     if (!req.session.user || req.session.user.role !== 'owner')
