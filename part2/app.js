179279bea4e5d3c62cf689018ddd6f355eaac1f5
a1936823 <lilokhang124@gmail.com>
2025-06-21 06:38:44 +0000

    [automatic commit] 2025-06-21 06:38:43 UTC

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,7 @@ app.use(
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('/routes/dogRoutes');
+const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
