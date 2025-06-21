e51371577b9a66ed3c39d2d4df87d422291f087e
a1936823 <lilokhang124@gmail.com>
2025-06-21 04:10:24 +0000

    [automatic commit] 2025-06-21 04:10:23 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,7 +68,10 @@ router.post('/login', async (req, res) => {
 });
 
 router.post(' /logout', (req, res)=> {
-  req.session.destroy
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid');
+    res.end()
+  })
 })
 
 
