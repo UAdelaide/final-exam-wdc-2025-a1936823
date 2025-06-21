fa62fb978eb7294e3ef5cc66401d3a2ecc505839
a1936823 <lilokhang124@gmail.com>
2025-06-21 06:09:54 +0000

    [automatic commit] 2025-06-21 06:09:53 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -3,15 +3,4 @@ const router = express.Router();
 const db = require('../models/db');
 const session = require('express-session');
 
-router,get('/mine', (req, res) => {
-    if (!req,session.user || req.session.user.role !== 'owner')
-        return res.status(401).json({error: 'Not authorized'});
-
-    db.query(
-        'SELECT dog_id AS id, name FROM Dogs WHERE onwer_id = ?',
-        [req.session.user.id]
-    )
-    .then(([rows]) => res.json(rows))
-    .catch(err => res.status(500).json({ error: 'DB error'}));
-    );
-    })
+router.get('/mine')
