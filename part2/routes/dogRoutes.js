0a5bf5227d279717a37a66d2e0a831ec35c547ef
a1936823 <lilokhang124@gmail.com>
2025-06-21 06:35:27 +0000

    [automatic commit] 2025-06-21 06:35:26 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -10,7 +10,9 @@ router.get('/mine', async(req, res) => {
     const ownerID = req.session.user.id;
 
     try{
-        const [rows] =
+        const [rows] = await db.query(
+            'SELECT dog_id, name, size FROM Dogs '
+        )
     }
 
 module.exports = router;
