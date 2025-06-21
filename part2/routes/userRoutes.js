9e3900c10d9cb435318b7874aaba474a19c08212
a1936823 <lilokhang124@gmail.com>
2025-06-21 02:50:07 +0000

    [automatic commit] 2025-06-21 02:50:07 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,9 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-router.post('/login', async (req, res)
+router.post('/login', async (req, res) =>{
+  const {username, password} = req.body;
+}
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
