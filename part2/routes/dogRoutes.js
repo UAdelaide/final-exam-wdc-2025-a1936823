52ef86057335ebef6b19ab761477cd38111c2f2b
a1936823 <lilokhang124@gmail.com>
2025-06-21 06:34:43 +0000

    [automatic commit] 2025-06-21 06:34:42 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -4,6 +4,8 @@ const db = require('../models/db');
 
 router.get('/mine', async(req, res) => {
     if (!req.session.user) {
-        return res.status(401).json
+        return res.status(401).json({ error: 'Not logged in;'});
     }
+    
+
 module.exports = router;
